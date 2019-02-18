aa192729e318930493ff79de01587148e588dbfc
Paulo Soares <pauloxti@gmail.com>
2019-02-18 23:51:53 +0100

    añadido pagina busca mi tienda

--- js/app.js
+++ js/app.js
@@ -1,27 +1,21 @@
 jQuery(function ($) {
 
-    // Function which adds the 'animated' class to any '.animatable' in view
     var animar = function () {
-
-        // Calc current offset and get all animatables
-        var offset = $(window).scrollTop() + $(window).height();
+        var rango = $(window).scrollTop() + $(window).height();
         var animable = $('.animable');
 
-        // Unbind scroll handler if we have no animatables
         if (animable.length === 0) {
             $(window).off('scroll', animar);
         }
 
-        // Check all animatables and animate them if necessary
         animable.each(function () {
             var animado = $(this);
-            if ((animado.offset().top + animado.height() - 20) < offset) {
+            if ((animado.offset().top + animado.height() - 20) < rango) {
                 animado.removeClass('animable').addClass('animado');
             }
         });
     };
 
-    // Hook doAnimations on scroll, and trigger a scroll
     $(window).on('scroll', animar);
     $(window).trigger('scroll');
 
